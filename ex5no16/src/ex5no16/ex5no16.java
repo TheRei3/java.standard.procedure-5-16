9a6147acc6b2dc05da8954dd77775ca82f4b4477
admin <admin@DESKTOP-8L2IG92>
2021-06-22 05:51:44 +0900

    ex5no16

---
+++ ex5no16/src/ex5no16/ex5no16.java
@@ -0,0 +1,11 @@
+package ex5no16;
+
+public class ex5no16 {  //  212p
+	public static void main(String[] args) {
+		System.out.println("매개변수의 개수 : "+args.length);
+		for( int i = 0 ; i < args.length ; i++ ) {
+			System.out.println("args[" + i + "] = \"" + args[i] + "\"");
+		} // for
+	}
+
+}
